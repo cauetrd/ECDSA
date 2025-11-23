d68d99c65aa7572b41d55865f14c5cd38ea50174
cauetrd <ctrindade2005@gmail.com>
2025-11-23 19:13:24 -0300

    init + file reading

---
+++ ecdsa/target/debug/deps/ecdsa.d
@@ -0,0 +1,5 @@
+C:\Users\ctrin\UnB\LP\ECDSA\ecdsa\target\debug\deps\ecdsa.d: src\main.rs
+
+C:\Users\ctrin\UnB\LP\ECDSA\ecdsa\target\debug\deps\ecdsa.exe: src\main.rs
+
+src\main.rs:
---
+++ ecdsa/target/debug/ecdsa.d
@@ -0,0 +1,1 @@
+C:\Users\ctrin\UnB\LP\ECDSA\ecdsa\target\debug\ecdsa.exe: C:\Users\ctrin\UnB\LP\ECDSA\ecdsa\src\main.rs
