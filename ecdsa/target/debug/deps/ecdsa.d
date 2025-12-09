e9de2944caa3882ea22743107300a97ab1d8c14e
rafaelghiorzi <rafael.ghiorzi@gmail.com>
2025-12-09 15:18:29 -0300

    estrutura inicial

--- ecdsa/target/debug/deps/ecdsa.d
+++
@@ -1,5 +0,0 @@
-C:\Users\ctrin\UnB\LP\ECDSA\ecdsa\target\debug\deps\ecdsa.d: src\main.rs
-
-C:\Users\ctrin\UnB\LP\ECDSA\ecdsa\target\debug\deps\ecdsa.exe: src\main.rs
-
-src\main.rs:
--- ecdsa/target/debug/ecdsa.d
+++
@@ -1,1 +0,0 @@
-C:\Users\ctrin\UnB\LP\ECDSA\ecdsa\target\debug\ecdsa.exe: C:\Users\ctrin\UnB\LP\ECDSA\ecdsa\src\main.rs
